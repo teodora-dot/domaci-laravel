59bab9b72de472b42527785b1db93abe7e131375
teodora-dot <78114920+teodora-dot@users.noreply.github.com>
2021-02-13 11:49:09 +0100

    Initial commit

---
+++ database/factories/IspitFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Ispit;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class IspitFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Ispit::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
